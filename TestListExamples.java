235e115e19e34f0bd400f5b021ab73777612b756
CaseyLoftus <114450184+CaseyLoftus@users.noreply.github.com>
2022-11-16 10:07:10 -0800

    asj

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,23 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testMerge() {
+    ArrayList<String> list1 = new ArrayList<>();
+    list1.add("a");
+    list1.add("c");
+    ArrayList<String> list2 = new ArrayList<>();
+    list2.add("b");
+    list2.add("d");
+    ArrayList<String> list3 = new ArrayList<>();
+    list3.add("a");
+    list3.add("b");
+    list3.add("c");
+    list3.add("d");
+    assertEquals(ListExamples.merge(list1, list2), list3);
+
+  }
 }
--- grade.sh
+++ grade.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
+if [[ ! -e ListExamples.java ]]
+then 
+    echo "ListExamples.java missing"
+fi
+cd ..
+cp TestListExamples.java ./student-submission
+cd ./student-submission
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+
 
